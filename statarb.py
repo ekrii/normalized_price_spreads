8585c75ae5d928b95ed1560ab1ef18228131cca6
EKRI <ekrii@protonmail.com>
2022-07-27 22:42:22 +1000

    _

--- statarb.py
+++ statarb.py
@@ -1,5 +1,14 @@
 import math
 import random
-import numpy
+import numpy 
 import yfinance as yf 
-import pandas 
+import pandas as pd
+
+msft = yf.Ticker('MSFT')
+print(msft)
+
+d = []
+for i in msft:
+    d.append(i)
+    x = pd.array(d)
+    print(x)
