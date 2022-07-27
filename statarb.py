ca78e5e03bcbdb5ad4b46f3a84f06cf1520bdfc1
EKRI <ekrii@protonmail.com>
2022-07-27 22:44:25 +1000

    def universe

--- statarb.py
+++ statarb.py
@@ -4,11 +4,6 @@ import numpy
 import yfinance as yf 
 import pandas as pd
 
-msft = yf.Ticker('MSFT')
-print(msft)
 
-d = []
-for i in msft:
-    d.append(i)
-    x = pd.array(d)
-    print(x)
+# define the asset class / universe
+
