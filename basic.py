902cfd2804f5c29f0525a6606ee115d9c5a695c1
EKRI <ekrii@protonmail.com>
2022-11-04 23:07:12 +1100

    none

--- basic.py
+++ basic.py
@@ -1,7 +1,2 @@
-from datetime import datetime
-from pandas_datareader import data
 import pandas as pd
-import numpy as np
-import seaborn as sns
-from collections import OrderedDict
-  
+import numpy as np 
